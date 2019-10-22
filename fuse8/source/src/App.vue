fb89056b7d91380d9843b5a243d1c9948f8924c1
Aleksandr Butenko <abutenko@aimax.pro>
2019-10-22 10:41:05 +0500

    add source

---
+++ fuse8/source/src/App.vue
@@ -0,0 +1,189 @@
+<template>
+    <div id="app" class="page">
+        <h1 class="title">{{title}}</h1>
+        <div class="item-wrap">
+            <item-list v-for="(elem, ind) in items"
+                       :key="ind"
+                       :items="items"
+                       :count="ind"
+            ></item-list>
+        </div>
+        <button v-show="btnShow" @click="showMore()" type="button" class="button-more">{{btnTitle}}</button>
+    </div>
+</template>
+
+<script>
+	import ItemList from './components/ItemList.vue'
+
+	export default {
+		name: 'app',
+		components: {
+			ItemList
+		},
+		data() {
+			return {
+				items: [
+					{
+						title: 'Brunlees Court',
+						address: '19-23 Cambridge Road, Southport',
+						saleText: 'New Properties for Sale from',
+						price: '£214,999',
+						option: 'Shared Ownership Available',
+						category: 'Independent living',
+						img: '/img/Image_1.jpg',
+						hide: false,
+					},
+					{
+						title: 'Charlotte Court',
+						address: '17-23 Cambridge Road, Southport',
+						saleText: 'New Properties for Sale from',
+						price: '£250,999',
+						option: 'Shared Ownership Available',
+						category: 'Independent living',
+						img: '/img/Image_2.jpg',
+						hide: false,
+					},
+					{
+						title: 'Grove Court',
+						address: '11-23 Cambridge Road, Southport',
+						saleText: 'New Properties for Sale from',
+						price: '£138,999',
+						option: 'Shared Ownership Available',
+						category: 'Restaurant & Support available',
+						img: '/img/Image_3.jpg',
+						hide: false,
+					},
+					{
+						title: 'Brunlees Court',
+						address: '19-23 Cambridge Road, Southport',
+						saleText: 'New Properties for Sale from',
+						price: '£214,999',
+						option: 'Shared Ownership Available',
+						category: 'Independent living',
+						img: '/img/Image_1.jpg',
+						hide: true
+					},
+					{
+						title: 'Charlotte Court',
+						address: '17-23 Cambridge Road, Southport',
+						saleText: 'New Properties for Sale from',
+						price: '£250,999',
+						option: 'Shared Ownership Available',
+						category: 'Independent living',
+						img: '/img/Image_2.jpg',
+						hide: true
+					},
+					{
+						title: 'Grove Court',
+						address: '11-23 Cambridge Road, Southport',
+						saleText: 'New Properties for Sale from',
+						price: '£138,999',
+						option: 'Shared Ownership Available',
+						category: 'Restaurant & Support available',
+						img: '/img/Image_3.jpg',
+						hide: true
+					},
+					{
+						title: 'Brunlees Court',
+						address: '19-23 Cambridge Road, Southport',
+						saleText: 'New Properties for Sale from',
+						price: '£214,999',
+						option: 'Shared Ownership Available',
+						category: 'Independent living',
+						img: '/img/Image_1.jpg',
+						hide: true
+					},
+					{
+						title: 'Charlotte Court',
+						address: '17-23 Cambridge Road, Southport',
+						saleText: 'New Properties for Sale from',
+						price: '£250,999',
+						option: 'Shared Ownership Available',
+						category: 'Independent living',
+						img: '/img/Image_2.jpg',
+						hide: true
+					},
+					{
+						title: 'Grove Court',
+						address: '11-23 Cambridge Road, Southport',
+						saleText: 'New Properties for Sale from',
+						price: '£138,999',
+						option: 'Shared Ownership Available',
+						category: 'Restaurant & Support available',
+						img: '/img/Image_3.jpg',
+						hide: true
+					},
+
+				],
+				title: 'Our Latest Developments',
+				btnTitle: 'See more',
+				btnShow: true
+			}
+		},
+		methods: {
+			showMore() {	              
+				let count = 0;
+				this.items.forEach((el,ind, arr) => {
+					if (count >= 3) return;
+					if (el.hide) {
+						el.hide = !el.hide;
+						count++;
+					}
+					if (ind+1 === arr.length) this.btnShow = !this.btnShow;
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="less">
+    .page {
+        max-width: 1180px;
+        margin: 0 auto;
+        padding: 57px 0 20px;
+        text-align: center;
+        font-family: "Open Sans";
+    }
+
+    .item-wrap {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+
+    .title {
+        margin: 0 0 43px;
+        font-weight: bold;
+        font-size: 36px;
+        color: #45852D;
+    }
+
+    .button-more {
+        margin-top: 30px;
+        padding: 0 12px 5px 0;
+        position:relative;
+        width: 150px;
+        height: 50px;
+        border: 1px solid #D8D8D8;
+        border-radius: 25px;
+        font-weight: bold;
+        text-align: center;
+        cursor:pointer;
+        transition: border 0.2s;
+        &:after {
+            content: '';
+            position:absolute;
+            top: 50%;
+            right: 29px;
+            transform: translateY(-50%);
+            width:6px;
+            height:17px;
+            z-index:1;
+            background-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNyIgaGVpZ2h0PSIxNyIgdmlld0JveD0iMCAwIDcgMTciIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggaWQ9IkNoZXZyb24iIGQ9Ik0xIDFMNS4wNDMgOC40MTY2N0wxIDE1LjgzMzMiIHN0cm9rZT0iIzM2MzYzNiIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiLz48L3N2Zz4=);
+        }
+        &:hover {
+            border:1px solid black;
+            transition: border 0.2s;
+        }
+    }
+</style>
---
+++ fuse8/source/src/components/ItemList.vue
@@ -0,0 +1,121 @@
+<template>
+    <div class="item" :class="isHide">
+        <a class="item__link" :href="'/page_'+count +''" :title="itemsArr.title"  target="_blank"></a>
+        <div class="item__img-wrap">
+            <img class="item__img" :src="itemsArr.img" :alt="itemsArr.title">
+            <div class="item__tag">{{itemsArr.category}}</div>
+        </div>
+
+        <div class="item__description">
+            <h2 class="item__title">{{itemsArr.title}}</h2>
+            <div class="item__address">{{itemsArr.address}}</div>
+            <div class="item__saleText">{{itemsArr.saleText}} <span class="item__price">{{itemsArr.price}}</span></div>
+            <div class="item__option">{{itemsArr.option}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+	export default {
+		props: {
+			items: Array,
+			count: Number,
+		},
+		computed: {
+			itemsArr() {
+				return {...this.items[this.count]}
+			},
+			isHide() {
+				return this.itemsArr.hide ? 'hide' : '';
+			},
+		},
+	}
+</script>
+
+<style lang="less">
+    .item {
+        position:relative;
+        text-align: left;
+        font-size: 16px;
+        &:hover {
+            .item__tag {
+                transition: background-color 0.2s;
+                background-color: #EC6608;
+            }
+        }
+        &.hide {
+            display: none;
+        }
+        &__link {
+            position: absolute;
+            top:0;
+            bottom:0;
+            left:0;
+            right:0;
+            z-index: 1;
+        }
+        &__img-wrap {
+            position: relative;
+            border-top: 1px solid #D8D8D8;
+            border-right: 1px solid #D8D8D8;
+            border-left: 1px solid #D8D8D8;
+        }
+        &__img {
+            width:100%;
+        }
+        &__tag {
+            padding: 8px 11px 5px 10px;
+            position: absolute;
+            bottom: 0;
+            right: 0;
+            font-size: 12px;
+            font-weight: bold;
+            line-height: 16px;
+            color: #fff;
+            background-color: #006F79;
+            transition: background-color 0.2s;
+        }
+        &__description {
+            padding: 20px 20px 16px;
+            margin-bottom: 20px;
+            border-right: 1px solid #D8D8D8;
+            border-bottom: 1px solid #D8D8D8;
+            border-left: 1px solid #D8D8D8;
+            line-height: 1.5;
+            background: #F8F8F8;
+        }
+        &__title {
+            font-size: 20px;
+            font-weight: bold;
+        }
+        &__address {
+            margin-bottom: 12px;
+        }
+        &__saleText {
+            margin-bottom:5px;
+        }
+        &__price {
+            font-weight: bold;
+        }
+        &__option {
+            font-size: 14px;
+        }
+    }
+    @media screen and (max-width:1230px) {
+        .item-wrap {
+            margin:0 auto;
+            max-width: 780px;
+        }
+    }
+    @media screen and (max-width:830px) {
+        .item-wrap {
+            margin:0 auto;
+            max-width: 380px;
+        }
+    }
+    @media screen and (max-width:769px) {
+        .item__description {
+            text-align: center;
+        }
+    }
+</style>
